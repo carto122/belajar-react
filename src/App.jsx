0b53f38760d7ab4ae80dc3fd109473de2a616b9a
carto122 <carto1221006@gmail.com>
2025-02-05 10:26:11 +0700

    init project pertama

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container card">
+        <h1>Belajar ReactJS</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+          fuga consectetur laudantium odio possimus magnam neque laborum ipsa
+          vel perferendis quo dolorum voluptatem repellat ipsum libero,
+          dignissimos expedita unde in!
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
