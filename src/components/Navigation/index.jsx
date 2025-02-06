e79fc4a2b289018dc2d4158621d7c3fa17fa6196
carto122 <carto1221006@gmail.com>
2025-02-06 23:27:32 +0700

    perubaan pada menu navigation

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -9,9 +9,14 @@ import { useState } from "react";
 
 const Navigation = () => {
   const [isOpenCatPanel, setIsOpenCatPanel] = useState(false);
+  const [openMenu, setOpenMenu] = useState(null);
 
-  const openCategoryPanel = () => {
-    setIsOpenCatPanel(true);
+  const handleMouseEnter = (menu) => {
+    setOpenMenu(menu);
+  };
+
+  const handleMouseLeave = () => {
+    setOpenMenu(null);
   };
 
   return (
@@ -21,7 +26,7 @@ const Navigation = () => {
           <div className="col_1 w-[30%]">
             <Button
               className="!text-[rgba(0,0,0,0.8)] gap-2 w-full !font-bold"
-              onClick={openCategoryPanel}
+              onClick={() => setIsOpenCatPanel(true)}
             >
               <RiMenu2Line className="text-[18px]" />
               Toko by categories
@@ -31,37 +36,35 @@ const Navigation = () => {
 
           <div className="col_1 w-[65%]">
             <ul className="flex items-center gap-3 !font-bold">
-              <li className="relative list-none">
-                <Link to="/" className="link transition text-[14px] font-[500]">
-                  <Button className="link transition !text-[rgba(0,0,0,0.8)] hover:!text-[#ff5252]">
-                    Home
-                  </Button>
-                </Link>
-              </li>
-              <li className="relative list-none">
+              <li
+                className="relative list-none"
+                onMouseEnter={() => handleMouseEnter("fashion")}
+                onMouseLeave={handleMouseLeave}
+              >
                 <Link to="/" className="link transition text-[14px] font-[500]">
                   <Button className="link transition !text-[rgba(0,0,0,0.8)] hover:!text-[#ff5252]">
                     Fashion
                   </Button>
-
+                </Link>
+                {openMenu === "fashion" && (
                   <div className="submenu absolute top-[120%] left-[0%] min-w-[200px] bg-white shadow-md transition-all">
                     <ul>
                       <li className="relative list-none w-full text-[12px]">
-                        <Link to="/" className="w-full">
+                        <Link to="/">
                           <button className="w-full cursor-pointer text-left px-2 py-2 transition hover:bg-gray-100 !text-[rgba(0,0,0,0.8)]">
                             Fashion Pria
                           </button>
                         </Link>
                       </li>
                       <li className="relative list-none w-full text-[12px]">
-                        <Link to="/" className="w-full">
+                        <Link to="/">
                           <button className="w-full cursor-pointer text-left px-2 py-2 transition hover:bg-gray-100 !text-[rgba(0,0,0,0.8)]">
                             Fashion Wanita
                           </button>
                         </Link>
                       </li>
                       <li className="relative list-none w-full text-[12px]">
-                        <Link to="/" className="w-full ">
+                        <Link to="/">
                           <button className="w-full cursor-pointer text-left px-2 py-2 transition hover:bg-gray-100 !text-[rgba(0,0,0,0.8)]">
                             Fashion Anak
                           </button>
@@ -69,8 +72,9 @@ const Navigation = () => {
                       </li>
                     </ul>
                   </div>
-                </Link>
+                )}
               </li>
+
               <li className="relative list-none">
                 <Link to="/" className="link transition text-[14px] font-[500]">
                   <Button className="link transition !text-[rgba(0,0,0,0.8)] hover:!text-[#ff5252]">
@@ -85,24 +89,9 @@ const Navigation = () => {
                   </Button>
                 </Link>
               </li>
-              <li className="relative list-none">
-                <Link to="/" className="link transition text-[14px] font-[500]">
-                  <Button className="link transition !text-[rgba(0,0,0,0.8)] hover:!text-[#ff5252]">
-                    Sayuran
-                  </Button>
-                </Link>
-              </li>
-              <li className="relative list-none">
-                <Link to="/" className="link transition text-[14px] font-[500]">
-                  <Button className="link transition !text-[rgba(0,0,0,0.8)] hover:!text-[#ff5252]">
-                    Kesehatan
-                  </Button>
-                </Link>
-              </li>
             </ul>
           </div>
 
-          {/* Info Gratis Ongkir */}
           <div className="col_1 w-[15%]">
             <p className="flex items-center gap-1 text-[14px] font-[500]">
               <FaTruckFast />
